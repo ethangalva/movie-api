33672e19782754030513677f8d6ce73bccdb7973
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-10-20 17:25:44 -0700

    added requirement for jwt key on the /movies endpoint of API

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 // [READ] - Return list of ALL movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
